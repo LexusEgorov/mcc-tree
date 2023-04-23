01b66bd8bab8adf2dd83e3ac963e10c070bff3fa
lexusegorov <alexey.egorov.job@gmail.com>
2023-04-24 00:44:08 +0700

    feat: add styles and handlers for tree-controls

--- src/components/tree-controls/tree-controls.tsx
+++ src/components/tree-controls/tree-controls.tsx
@@ -1,9 +1,54 @@
 import './tree-controls.css';
 
 function TreeControls() : JSX.Element {
+  /*
+    Handlers for Add, Remove, Edit, Reset
+    OnClick: openModal(action, selectedNode)
+  */
+  const handleAdd = () => {
+    console.log('add');
+  };
+  
+  const handleRemove = () => {
+    console.log('remove');
+  };
+  
+  const handleEdit = () => {
+    console.log('edit');
+  };
+  
+  const handleReset = () => {
+    console.log('reset');
+  };
+
   return(
     <article className="tree-controls">
-      tree-controls
+      <div className="controls">
+        <button
+          className='control'
+          onClick={handleAdd}
+        >
+          Add
+        </button>
+        <button
+          className='control'
+          onClick={handleRemove}
+        >
+          Remove
+        </button>
+        <button
+          className='control'
+          onClick={handleEdit}
+        >
+          Edit
+        </button>
+        <button
+          className='control'
+          onClick={handleReset}
+        >
+          Reset
+        </button>
+      </div>
     </article>
   );
 }
